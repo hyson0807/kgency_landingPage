847e8058eccb82c21caa67b7245bd38ac26e0d30
hyson0807 <simsgood0807@gmail.com>
2025-09-29 19:45:37 +0900

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
-  import path from 'path';
+  import * as path from 'path';
 
   export default defineConfig({
     plugins: [react()],
@@ -56,7 +56,7 @@
     },
     build: {
       target: 'esnext',
-      outDir: 'build',
+      outDir: 'dist',
     },
     server: {
       port: 3000,
